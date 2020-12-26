9b339b19a940dc0828270e71e9de5c3dfee73482
Luis Miguel Souza <68262792+luismiguwl@users.noreply.github.com>
2020-12-26 07:05:44 -0300

    Primeiro commit

---
+++ campo-minado/src/br/com/cod3r/cm/visao/Aplicacao.java
@@ -0,0 +1,12 @@
+import br.com.cod3r.cm.modelo.Tabuleiro;
+import br.com.cod3r.cm.visao.TabuleiroConsole;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+		new TabuleiroConsole(tabuleiro);
+
+	}
+
+}
